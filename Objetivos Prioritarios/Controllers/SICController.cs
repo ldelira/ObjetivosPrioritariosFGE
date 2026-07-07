5e1212f44a59b2918aae7c8857d5c5f83f2d14d8
APadillaA <APadillaA@143FGDP>
2026-07-07 15:48:03 -0600

    cambios filiacion municipal poncho

---
+++ Objetivos Prioritarios/Controllers/SICController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Objetivos_Prioritarios.Models;
+
+namespace Objetivos_Prioritarios.Controllers
+{
+    public class SICController : Controller
+    {
+        #region "Vista principal SIC"
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "SIC";
+            return View();
+        }
+
+        #endregion
+
+
+        #region "Obtener información SIC"
+
+        [HttpGet]
+        public JsonResult ObtenerSIC()
+        {
+            try
+            {
+                var data = new List<object>
+                {
+                    new
+                    {
+                        Id = 1,
+                        Nombre = "JUAN PÉREZ LÓPEZ",
+                        Fuente = "SIC",
+                        Estatus = "ACTIVO",
+                        Detenciones = 2
+                    },
+                    new
+                    {
+                        Id = 2,
+                        Nombre = "PEDRO RAMÍREZ GARCÍA",
+                        Fuente = "SIC",
+                        Estatus = "INACTIVO",
+                        Detenciones = 0
+                    }
+                };
+
+                return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    data = new List<object>(),
+                    error = ex.Message
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        #endregion
+    }
+}
